c7779b837b1ea0b2c2ead5620f9afe60d234c5cd
Mark <darththienan@gmail.com>
2012-04-16 00:50:25 -0400

    commented

--- src/com/connect/Connect.java
+++ src/com/connect/Connect.java
@@ -1,5 +1,9 @@
 /**
  * Remember that AndroidManifest.xml needs to give permission for connectivity
+ * 
+ * Enter lines into AndroidManifest, directly under the <manifest> tag:
+ * <uses-permission android:name="android.permission.INTERNET" />
+ * <uses-permission android:name="android.permission.ACCESS_WIFI_STATE" />
  */
 
 package com.connect;
@@ -16,14 +20,12 @@ import java.net.Socket;
 import java.net.SocketException;
 import java.util.Enumeration;
 
-import android.os.Handler;
-import android.util.Log;
-import android.widget.TextView;
-
 public class Connect {
 
 	/** default port **/
-	public static final int SERVERPORT = 8080;
+	private static final int SERVERPORT = 8080;
+	private int serverPort = 0;
+	
 
 	/** input stream from server **/
 	BufferedReader serverIn;
@@ -33,135 +35,31 @@ public class Connect {
 	BufferedReader clientIn;
 	/** output stream from client **/
 	PrintWriter clientOut;
-	/** server socket that will initialize other sockets **/
+	/** server phone's IP **/
 	String serverIP = "";
 	/** server socket that will initialize other sockets **/
 	ServerSocket srvSocket;
 	/** whether or not the client is connected yet **/
 	boolean connected;
 	
-	//for testing
-	TextView text;
-	Handler handler = new Handler();
-
-//	@Override
-//	protected void onCreate(Bundle savedInstanceState) {
-//		super.onCreate(savedInstanceState);
-//
-//		// remove title bar
-//		requestWindowFeature(Window.FEATURE_NO_TITLE);
-//
-//		setContentView(R.layout.main);
-//		text = (TextView) findViewById(R.id.main_text);
-//	}
-//
-//	@Override
-//	public boolean onKeyDown(int keyCode, KeyEvent msg) {
-//		
-//		String ip = "";
-//		
-//		if (testCount == 0)
-//		{
-//			initServer();
-//			text.setText("Server initialized");
-//		}
-//		if (testCount == 1)
-//		{
-//			ip = getServerIP();
-//			text.setText("Server IP found: " + ip);
-//		}
-//		if (testCount == 2)
-//		{
-//			if(initClient(ip))
-//				text.setText("Client initialized");
-//			else
-//				text.setText("Client initialization failed");
-//		}
-//		if (testCount == 3)
-//		{
-//			sendMsgFromServer("Greetings");
-//			text.setText("Message from Server Sent : ?");
-//		}
-//		if (testCount == 4)
-//		{
-//			String sMsg = getMsgToClient();
-//			text.setText("Message from Server Sent : " + sMsg);
-//		}
-//		if (testCount == 5)
-//		{
-//			sendMsgFromClient("And hello to you!");
-//			text.setText("Message from Client Sent : ?");
-//		}
-//		if (testCount == 6)
-//		{
-//			String cMsg = getMsgToServer();
-//			text.setText("Message from Client Sent : " + cMsg);
-//		}
-//		if (testCount == 7)
-//		{
-//			close();
-//			text.setText("Socket Closed");
-//		}
-//		
-//		testCount++;
-//		
-//		return super.onKeyDown(keyCode, msg);
-//	}	
-//	@Override
-//	protected void onStop() {
-//		super.onStop();
-//
-//		System.exit(0);
-//	}
-
+	/** Initialize Server Socket, begin listening for connections **/
 	public void initServer()
 	{
 		Thread serverThread = new Thread(new ServerThread());
 		serverThread.start();
 		serverIP = getLocalIpAddress();
-
-//		try {
-//			if (serverIP != null) {
-//
-//				//open server socket
-//				srvSocket = new ServerSocket(SERVERPORT);
-//				handler.post(new Runnable() {
-//					@Override
-//					public void run() {
-//						text.setText("2");
-//					}
-//				});
-//
-//				while (true) {
-//					//begin listening, accept when it comes
-//					Socket client = srvSocket.accept();
-//
-//					//init I/O streams
-//					try {
-//						serverIn = new BufferedReader(new InputStreamReader(client.getInputStream()));
-//						// set true, for auto-flushing after print statements
-//						serverOut = new PrintWriter(new BufferedWriter(new OutputStreamWriter(client.getOutputStream())), true);
-//
-//						handler.post(new Runnable() {
-//							@Override
-//							public void run() {
-//								text.setText("3");
-//							}
-//						});
-//						
-//						break;
-//					} catch (Exception e) {
-//						e.printStackTrace();
-//					}
-//				}
-//			} else {
-//				//couldn't detect
-//			}
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
 	}
 
+	/** Initialize Server Socket with specific port, begin listening for connections **/
+	public void initServer(int port)
+	{
+		serverPort = port;
+		Thread serverThread = new Thread(new ServerThread());
+		serverThread.start();
+		serverIP = getLocalIpAddress();
+	}
+
+	/** Close the Server Socket and end connections **/
 	public void close()
 	{
 		try {
@@ -169,21 +67,31 @@ public class Connect {
 		}
 		catch (Exception e)
 		{
+			//Terminate application if close socket fails
 			System.exit(0);
 		}
 	}
 	
+	/** Returns the server phone's IP address 
+	 * 
+	 * @return: server phone's IP address
+	 * **/
 	public String getServerIP()
 	{
 		return serverIP;
 	}
 	
+	/** If Server's output stream has been initialized, send "msg" **/
 	public void sendMsgFromServer(String msg)
 	{
 		if (serverOut != null)
 			serverOut.println(msg);
 	}
 	
+	/** If Server's input stream has been initialized, read from input stream 
+	 * 
+	 * @return: Return message from Client.
+	 * **/
 	public String getMsgToServer()
 	{
 		try {
@@ -193,10 +101,15 @@ public class Connect {
 			return sendValue;
 		}
 		catch (Exception e) {
+			//return null if no message found
 			return null;
 		}
 	}
 
+	/** Initialize Client Socket, attempt to connect to give IP address "ip" 
+	 * 
+	 * @return: true if initialization succeeds, false if initializations fails
+	 * **/
 	public boolean initClient(String ip)
 	{
 		if (!connected) {
@@ -209,47 +122,40 @@ public class Connect {
 			
 			return true;
 		} else
-		{
-			//error
-		}
-
-		
-//		try {
-//			if (!connected) {
-//				InetAddress serverAddr = InetAddress.getByName(ip);
-//				// connecting
-//				Socket server = new Socket(serverAddr, SERVERPORT);
-//				if (server != null)
-//					connected = true;
-//
-//				while (connected) {
-//					try {
-//						clientIn = new BufferedReader(new InputStreamReader(server.getInputStream()));
-//						// set true, for auto-flushing after print statements
-//						clientOut = new PrintWriter(new BufferedWriter(new OutputStreamWriter(server.getOutputStream())), true);
-//						
-//						return true;
-////						
-////						break;
-//					} catch (Exception e) {
-//						e.printStackTrace();
-//					}
-//				}
-//			} else {
-//				//couldn't detect
-//			}
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-		return false;
+			return false;
 	}
 	
+	/** Initialize Client Socket, attempt to connect to give IP address "ip" 
+	 * 
+	 * @return: true if initialization succeeds, false if initializations fails
+	 * **/
+	public boolean initClient(String ip, int port)
+	{
+		if (!connected) {
+			
+			serverIP = ip;
+			serverPort = port;
+			if (!ip.equals("")) {
+				Thread cThread = new Thread(new ClientThread());
+				cThread.start();
+			}
+			
+			return true;
+		} else
+			return false;
+	}
+	
+	/** If Client's output stream has been initialized, send "msg" **/
 	public void sendMsgFromClient(String msg)
 	{
 		if (clientOut != null)
 			clientOut.println(msg);
 	}
 	
+	/** If Server's input stream has been initialized, read from input stream 
+	 * 
+	 * @return: Return message from Client.
+	 * **/
 	public String getMsgToClient()
 	{
 		try {
@@ -263,6 +169,7 @@ public class Connect {
 		}
 	}
 	
+	/** Function to find phone's IP Address **/
 	private String getLocalIpAddress() {
 		try {
 			for (Enumeration<NetworkInterface> en = NetworkInterface
@@ -282,48 +189,35 @@ public class Connect {
 		return null;
 	}
 	
-	public class ServerThread implements Runnable {
+	/** Create a separate thread to create server socket connection **/
+	private class ServerThread implements Runnable {
 		public void run() {
 			try {
 				if (serverIP != null) {
 					
-					//open server socket
-					srvSocket = new ServerSocket(SERVERPORT);
-					
-//					handler.post(new Runnable() {
-//						@Override
-//						public void run() {
-//							text.setText("Listening on IP: " + serverIP);
-//						}
-//					});
+					//check if a specific port was defined
+					if (serverPort != 0)
+						srvSocket = new ServerSocket(serverPort);
+					//else, open server socket with default port
+					else
+						srvSocket = new ServerSocket(SERVERPORT);
+
 
 					while (true) {
 						//begin listening, accept when it comes
 						Socket client = srvSocket.accept();
 
-//						handler.post(new Runnable() {
-//							@Override
-//							public void run() {
-//								text.setText("2");
-//							}
-//						});
-						
 						//init I/O streams
 						try {
+							//initialize server input stream
 							serverIn = new BufferedReader(new InputStreamReader(client.getInputStream()));
+							//initialize server output stream
 							// set true, for auto-flushing after print statements
 							serverOut = new PrintWriter(new BufferedWriter(new OutputStreamWriter(client.getOutputStream())), true);
 	
-//							handler.post(new Runnable() {
-//								@Override
-//								public void run() {
-//									text.setText("3");
-//								}
-//							});
-							
 							break;
 						} catch (Exception e) {
-							Log.d("ServerInitThread", "FAIL");
+							//Server Initialization fails
 							e.printStackTrace();
 						}
 					}
@@ -336,61 +230,43 @@ public class Connect {
 		}
 	}
 	
-	public class ClientThread implements Runnable {
+	/** Create a separate thread to create client socket connection **/
+	private class ClientThread implements Runnable {
 		public void run() {
 			try {
+				//only try if not already connected
 				if (!connected) {
-					InetAddress serverAddr = InetAddress
-							.getByName(serverIP);
-					// connecting
-					Socket server = new Socket(serverAddr, SERVERPORT);
+					Socket server;
+					InetAddress serverAddr = InetAddress.getByName(serverIP);
+
+					//try to connect to a specific port, if given
+					if (serverPort != 0)
+						server = new Socket(serverAddr, serverPort);
+					//else, open socket with default port
+					else
+						server = new Socket(serverAddr, SERVERPORT);
+					
 					if (server != null)
 						connected = true;
 
 					while (connected) {
-
-//						handler.post(new Runnable() {
-//							@Override
-//							public void run() {
-//								setContentView(R.layout.client2);
-//
-//								clientStatus = (TextView) findViewById(R.id.client2_status);
-//								clientMsg = (EditText) findViewById(R.id.client2_msg);
-//								clientStatus.setText("Waiting for Message");
-//
-//								clientStatus.setText("Connected");
-//							}
-//						});
-
 						try {
+							//initialize client input stream
 							clientIn = new BufferedReader(new InputStreamReader(server.getInputStream()));
+							//initialize client output stream
 							// set true, for auto-flushing after print statements
-
 							clientOut = new PrintWriter(new BufferedWriter(new OutputStreamWriter(server.getOutputStream())), true);
-
 						} catch (Exception e) {
 							e.printStackTrace();
 						}
 					}
 				} else {
-//					handler.post(new Runnable() {
-//						@Override
-//						public void run() {
-//							text.setText("Couldn't detect");
-//						}
-//					});
+					//connected already
+					return;
 				}
 			} catch (Exception e) {
-//				handler.post(new Runnable() {
-//					@Override
-//					public void run() {
-//						text.setText("ERROR");
-//					}
-//				});
 				e.printStackTrace();
 			}
 		}
 	}
-	
-	
 }
